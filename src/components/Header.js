68fbb5a2a75b1381019f04e53a48ddaa33c71058
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2023-11-07 08:45:03 +0530

    app set-up

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between'>
+        <div>
+          <img src="./asset/logo.jpg" alt="logo" />
+        </div>
+
+        <div>
+          <h1>Hello</h1>
+        </div>
+
+        <div>
+          <h1>Hello</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
