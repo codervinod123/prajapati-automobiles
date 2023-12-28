068ec37e817e4c8ff28cb32893b320932cb8ca85
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2023-12-28 16:09:15 +0530

    feat/shimmer design fixed

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -6,12 +6,12 @@ const Shimmer=()=>{
      const shimmerarray=Array.from({length:36});
     
 return(
-    <div className='flex flex-wrap gap-8 m-4 justify-center bg-white'>
+    <div className='w-full grid grid-cols-3 gap-x-4 gap-y-8 '>
      {
         shimmerarray.map((data,index)=>{
             return(
-                 <div key={index} className="w-full grid grid-cols-3 gap-x-4 gap-y-8">
-                    <div className='skeleton h-32 rounded'> </div>
+                 <div key={index} className="w-full">
+                    <div className='skeleton h-64 rounded'> </div>
                  </div>
             )
         })
